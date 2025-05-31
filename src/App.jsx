55e5d3ff0210f59d2d2e7c6903072a6284f100c6
Racingsoul28 <diegoperdomo532@gmail.com>
2025-05-31 06:02:36 -0600

    desinstalarReactRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import LoginContainer from "./loginContainer";
 import HomePage from "./homepage";
 import AboutUs from "./aboutUs";
-import { Link, BrowserRouter, Route } from "react-router";
 import "./index.css";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Link to="/">Login</link>
-        <Route>
-          <LoginContainer />
-        </Route>
-      </BrowserRouter>
+      <h1>Hola mundo2</h1>
     </>
   );
 }
