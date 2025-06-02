3117bb785b584a9418763ae5995a344c0b6e693a
racingsoul28 <diegoperdomo532@gmail.com>
2025-06-01 20:04:09 -0600

    Revertir cambios y volver al estado del primer commit

---
+++ src/aboutUs.jsx
@@ -0,0 +1,18 @@
+function AboutUs(props) {
+    return(
+        <>
+            <div>
+                <h1>Sobre Nosotros</h1>
+                <p>
+                    Bienvenido a mi sitio web. Soy un desarrollador independiente apasionado por crear soluciones de software innovadoras y eficientes para entornos web con un enfoque especializado en redes locales. <br/>
+                    Mi misión es transformar ideas en aplicaciones sólidas y funcionales que optimicen procesos y mejoren la conectividad interna de empresas y organizaciones. Con experiencia en tecnologías web y un enfoque centrado en la estabilidad y el rendimiento, garantizo soluciones adaptadas a tus necesidades. <br/>
+                    Cada proyecto es una oportunidad para aportar creatividad, experiencia y compromiso. Si buscas desarrollo de software confiable, escalable y personalizado, estás en el lugar indicado.<br/>
+                    ¡Trabajemos juntos para hacer realidad tu visión!<br/>
+                    Si deseas personalizar más el tono o incluir detalles específicos sobre tu experiencia y habilidades, dime qué aspectos quieres destacar.<br/>
+                </p>
+            </div>
+        </>
+    );
+}
+
+export default AboutUs;
