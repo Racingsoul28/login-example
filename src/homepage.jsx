90d02dcedc332772e510c69b2146e777adff0551
Racingsoul28 <diegoperdomo532@gmail.com>
2025-06-10 12:44:38 -0600

    se agregó el inicio de sesión

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -2,9 +2,9 @@ function HomePage(props) {
     return(
         <>
             <div className="homePage">
+                <button onClick={props.onlogout}>Cerrar sesión</button>
                 <h1>Bienvenido a la Página Principal</h1>
                 <p>Esta es la página de inicio de tu aplicación.</p>
-                <button onClick={props.onLogout}>Cerrar sesión</button>
             </div>
         </>
     );
--- src/login.jsx
+++ src/login.jsx
@@ -1,32 +1,44 @@
-import "./loginContainer.css"
+import "./loginContainer.css";
+import "./pressIntro";
+import PressIntro from "./pressIntro";
 
 function Login(props) {
-    return (
-        <>
-            <div className="loginContainer">
-                <h1>Bienvenido</h1>
-                <div className="loginInput">
-                    <p>Usuario:</p>
-                    <input
-                        type="text"
-                        name="impUser"
-                        id="impUser"
-                    />
-                </div>
-                <div className="loginInput">
-                    <p>Contraseña:</p>
-                    <input
-                        type="password"
-                        name="inpPwd"
-                        id="inpPwd"
-                    />
-                </div>
-                <button type="button">
-                    Iniciar sesión
-                </button>
-            </div>
-        </>
-    );
+  const lcUser = "admin";
+  const lcPwd = "S3cureP@ssw0rd";
+
+  const handleLogin = () => {
+    var user = document.getElementById("impUser").value;
+    var pwd = document.getElementById("inpPwd").value;
+    if (user === lcUser && pwd === lcPwd) {
+      props.onLogin();
+    } else {
+      alert("Usuario o contraseña incorrectos");
+    }
+  };
+
+  return (
+    <>
+      <div className="loginContainer">
+        <h1>Bienvenido</h1>
+        <div className="loginInput">
+          <p>Usuario:</p>
+          <input type="text" name="impUser" id="impUser" />
+        </div>
+        <div className="loginInput">
+          <p>Contraseña:</p>
+          <input
+            type="password"
+            name="inpPwd"
+            id="inpPwd"
+            onKeyDown={PressIntro(handleLogin)}
+          />
+        </div>
+        <button type="button" onClick={handleLogin}>
+          Iniciar sesión
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default Login;
