3dda6f363708b1d1fadcd6c9ea99468738795fde
Racingsoul28 <diegoperdomo532@gmail.com>
2025-06-14 17:06:03 -0600

    avances en la interfaz de homepage

--- src/card.jsx
+++ src/card.jsx
@@ -1,18 +1,22 @@
 import "./card.css";
 
 function Card(props) {
-    let { title, content } = props;
-    if (!title) title = "Título por defecto";
-    if (!content) content = "Contenido por defecto";
+  let { title, content, imgCard } = props;
+  if (!title) title = "Título por defecto";
+  if (!content) content = "Contenido por defecto";
+  if (!imgCard) imgCard = "https://th.bing.com/th/id/OIP.pwMO2wypxophplLz-UqqrgHaHa?o=7rm=3&rs=1&pid=ImgDetMain&cb=idpwebpc1";
 
-    return(
-        <>
-            <div className="card">
-                <h2>{title}</h2>
-                <p>{content}</p>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="card">
+        <div className="imgContainer">
+          <img src={imgCard} alt="moduleImg" className="cardImg" />
+        </div>
+        <h2 className="cardTittle">{title}</h2>
+        <p className="cardDescription">{content}</p>
+      </div>
+    </>
+  );
 }
 
 export default Card;
