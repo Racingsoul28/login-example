76be5bcf0535127228c489e244fca05287febdaa
Racingsoul28 <diegoperdomo532@gmail.com>
2025-06-26 06:48:32 -0600

    vite conf 10

--- src/homepage.jsx
+++ src/homepage.jsx
@@ -51,28 +51,28 @@ function HomePage(props) {
               <Card
                 title="Administración"
                 content="Aplicativo para llevar el control de la administración del negocio"
-                imgCard="/homePageImg/ico_AdminV02.png"
+                imgCard="./homePageImg/ico_AdminV02.png"
               />
               <Card
                 title="Inventario"
                 content="Aplicativo para llevar el control del inventario de los insumos"
-                imgCard="/homePageImg/ico_Modulev03.png"
+                imgCard="./homePageImg/ico_Modulev03.png"
               />
               <Card
                 title="Configuración"
                 content="Configuraciones generales del sistema"
-                imgCard="/homePageImg/ico_ConfigV02.png"
+                imgCard="./homePageImg/ico_ConfigV02.png"
               />
 
               <Card
                 title="Administración"
                 content="Aplicativo para llevar el control de la administración del negocio"
-                imgCard="/homePageImg/ico_AdminV02.png"
+                imgCard="./homePageImg/ico_AdminV02.png"
               />
               <Card
                 title="Inventario"
                 content="Aplicativo para llevar el control del inventario de los insumos"
-                imgCard="/homePageImg/ico_Modulev03.png"
+                imgCard="./homePageImg/ico_Modulev03.png"
               />
             </div>
           </main>
